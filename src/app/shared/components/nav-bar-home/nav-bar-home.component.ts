a1da537cc087d7fc57191aa32cc42afbdcd6f55b
JavierS <javieandres2004@gmail.com>
2025-04-25 19:09:22 -0500

    upload: first commit

---
+++ src/app/shared/components/nav-bar-home/nav-bar-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-home',
+  imports: [],
+  templateUrl: './nav-bar-home.component.html',
+  styleUrl: './nav-bar-home.component.css'
+})
+export class NavBarHomeComponent {
+
+}
