bbb030600102b9f3548ab850d77a9cfc608a8213
Carlos Romero <romero.ca20042018@gmail.com>
2025-05-04 13:22:46 -0500

    registerStudents

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { NavBarHomeComponent } from '../../shared/components/nav-bar-home/nav-bar-home.component';
+import { Router } from '@angular/router'; // Asegúrate de importar esto
 import { ButtonModule } from 'primeng/button';
+import { NavBarHomeComponent } from '../../shared/components/nav-bar-home/nav-bar-home.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true, // si estás usando Angular 17+ con componentes standalone
   imports: [NavBarHomeComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
