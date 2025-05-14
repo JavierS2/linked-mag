1716036229007ea353c6ff2777946fc1b1ee1d65
Carlos Romero <romero.ca20042018@gmail.com>
2025-05-13 23:26:26 -0500

    advance dashboard-student

--- src/app/pages/dashboard-student/dashboard-student.component.ts
+++ src/app/pages/dashboard-student/dashboard-student.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { SidebarStudentComponent } from '../../shared/components/sidebar-student/sidebar-student.component';
-
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { InputTextModule } from 'primeng/inputtext';
+import { AvatarModule } from 'primeng/avatar';
+import { CardModule } from 'primeng/card';
+import { RippleModule } from 'primeng/ripple';
+import { DashboardCompanyComponent } from "../dashboard-company/dashboard-company.component";
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
 @Component({
   selector: 'app-dashboard-student',
-  imports: [SidebarStudentComponent],
+  imports: [SidebarStudentComponent, MenubarModule, BadgeModule,
+    InputTextModule, AvatarModule, RippleModule, CardModule, PanelModule,
+    ButtonModule],
   templateUrl: './dashboard-student.component.html',
   styleUrl: './dashboard-student.component.css'
 })
