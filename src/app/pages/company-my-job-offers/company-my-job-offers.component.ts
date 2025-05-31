b95efd1925d63b536bfad4049196186998a1b25d
JavierS <javieandres2004@gmail.com>
2025-05-31 18:46:17 -0500

    feat: Add show offers

--- src/app/pages/company-my-job-offers/company-my-job-offers.component.ts
+++ src/app/pages/company-my-job-offers/company-my-job-offers.component.ts
@@ -15,6 +15,8 @@ import { TagModule } from 'primeng/tag';
 import { IconFieldModule } from 'primeng/iconfield';
 import { MenubarModule } from 'primeng/menubar';
 import { RouterLink } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { JobOffer } from '../../models/job-offer';
 
 @Component({
   selector: 'app-company-my-job-offers',
@@ -24,81 +26,26 @@ import { RouterLink } from '@angular/router';
   templateUrl: './company-my-job-offers.component.html',
   styleUrl: './company-my-job-offers.component.css'
 })
-export class CompanyMyJobOffersComponent {
-  loading = false;
 
-  jobOffers = [
-    {
-      name: 'Desarrollador Frontend',
-      email: 'frontend@empresa.com',
-      phone: 123456789,
-      date: new Date('2024-06-01'),
-      city: 'Madrid',
-      country: 'España',
-      modality: 'Presencial',
-      salary: 30000,
-      description: 'Desarrollo de aplicaciones web con Angular.',
-      status: 'Abierta'
-    },
-    {
-      name: 'Ingeniero Backend',
-      email: 'backend@empresa.com',
-      phone: 987654321,
-      date: new Date('2024-05-15'),
-      city: 'Barcelona',
-      country: 'España',
-      modality: 'Remoto',
-      salary: 35000,
-      description: 'Desarrollo de APIs y microservicios con Node.js.',
-      status: 'Cerrada'
-    },
-    {
-      name: 'Diseñador UX/UI',
-      email: 'uxui@empresa.com',
-      phone: null,
-      date: new Date('2024-06-10'),
-      city: 'Valencia',
-      country: 'España',
-      modality: 'Híbrido',
-      salary: 28000,
-      description: 'Diseño de interfaces y experiencia de usuario.',
-      status: 'Pausada'
-    },
-    {
-      name: 'Diseñador UX/UI',
-      email: 'uxui@empresa.com',
-      phone: null,
-      date: new Date('2024-06-10'),
-      city: 'Bogotá',
-      country: 'Colombia',
-      modality: 'Híbrido',
-      salary: 15000,
-      description: 'Diseño de interfaces y experiencia de usuario.',
-      status: 'Pausada'
-    },
-    {
-      name: 'Desarrollador Frontend',
-      email: 'frontend@empresa.com',
-      phone: 123456789,
-      date: new Date('2024-06-01'),
-      city: 'Madrid',
-      country: 'España',
-      modality: 'Presencial',
-      salary: 30000,
-      description: 'Desarrollo de aplicaciones web con Angular.',
-      status: 'Abierta'
-    },
-    {
-      name: 'Desarrollador Frontend',
-      email: 'frontend@empresa.com',
-      phone: 123456789,
-      date: new Date('2024-06-01'),
-      city: 'Madrid',
-      country: 'España',
-      modality: 'Presencial',
-      salary: 30000,
-      description: 'Desarrollo de aplicaciones web con Angular.',
-      status: 'Cerrada'
-    },
-  ];
+export class CompanyMyJobOffersComponent {
+
+  constructor(private api: ApiService) { }
+
+  loading = false;
+  jobOffers: JobOffer[] = [];
+
+  ngOnInit() {
+    this.loadOffers();
+  }
+
+  loadOffers() {
+    this.api.getAllOffers().subscribe({
+      next: (offers: JobOffer[]) => {
+        this.jobOffers = offers;
+      },
+      error: (err) => {
+        console.error('Error al cargar ofertas', err);
+      }
+    });
+  }
 }
